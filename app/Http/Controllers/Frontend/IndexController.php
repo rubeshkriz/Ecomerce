a30b8aa027d267a8dd114243e3568dd74fedc3bd
rubeshkrishnan <rubesh.indus@com>
2022-05-17 10:32:08 +0530

    FrontEnd Display Products By Index

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Auth;
 use App\Models\User;
 use App\Models\Category;
+use App\Models\Brand;
 use App\Models\Slider;
 use App\Models\Product;
 use App\Models\MultiImg;
@@ -19,7 +20,25 @@ class IndexController extends Controller
         $products = Product::where('status',1)->orderBy('id','DESC')->limit(6)->get();
         $sliders = Slider::where('status',1)->orderBy('id','DESC')->limit(3)->get();
         $categories = Category::orderBy('category_name_en','ASC')->get();
-        return view('frontend.index',compact('categories','sliders','products'));
+        $featured = Product::where('featured',1)->orderBy('id','DESC')->limit(6)->get();
+        $hot_deals = Product::where('hot_deals',1)->where('discount_price','!=',NULL)->orderBy('id','DESC')->limit(3)->get();
+        $special_offer = Product::where('special_offer',1)->orderBy('id','DESC')->limit(9)->get();
+        $special_deals = Product::where('special_deals',1)->orderBy('id','DESC')->limit(9)->get();
+
+        $skip_category_0 = Category::skip(0)->first();
+        $skip_product_0 = Product::where('status',1)->where('category_id',$skip_category_0->id)->orderBy('id','DESC')->get();
+
+        $skip_category_1 = Category::skip(1)->first();
+        $skip_product_1 = Product::where('status',1)->where('category_id',$skip_category_1->id)->orderBy('id','DESC')->get();
+
+        $skip_brand_2 = Brand::skip(2)->first();
+        $skip_brand_product_2 = Product::where('status',1)->where('brand_id',$skip_brand_2->id)->orderBy('id','DESC')->get();
+        // return $skip_category->id;
+        // die();
+
+        return view('frontend.index',compact('categories','sliders','products','featured',
+        'hot_deals','special_offer','special_deals','skip_category_0','skip_product_0',
+        'skip_category_1','skip_product_1','skip_brand_2','skip_brand_product_2'));
     }
 
     public function UserLogout(){
