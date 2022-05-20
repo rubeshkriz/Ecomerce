6f66f9386260b72352661f840ed327990167fdd1
rubeshkrishnan <rubesh.indus@com>
2022-05-20 23:11:30 +0530

    Checkout Page CompletedFully FRONTEND

--- app/Http/Controllers/User/checkoutController.php
+++ app/Http/Controllers/User/checkoutController.php
@@ -22,4 +22,24 @@ class checkoutController extends Controller
 
         return json_encode($ship);
     }
+
+    public function CheckoutStore(Request $request){
+        $data = array();
+        $data['shipping_name'] = $request->shipping_name;
+        $data['shipping_email'] = $request->shipping_email;
+        $data['shipping_phone'] = $request->shipping_phone;
+        $data['post_code'] = $request->post_code;
+        $data['division_id'] = $request->division_id;
+        $data['district_id'] = $request->district_id;
+        $data['state_id'] = $request->state_id;
+        $data['notes'] = $request->notes;
+
+        if($request->payment_method == 'stripe'){
+            return view('frontend.payment.stripe',compact('data'));
+        }elseif($request->payment_method == 'card'){
+            return 'card';
+        }else{
+            return 'cash';
+        }
+    }
 }
---
+++ resources/views/frontend/payment/stripe.blade.php
@@ -0,0 +1,2 @@
+<h1>Stipe Is </h1>
+<div>{{ $data['shipping_name'] }}</div>
