e0efdf153ed869e50a0dacda764d6350b14e5f7b
rubeshkrishnan <rubesh.indus@com>
2022-05-22 12:59:06 +0530

    myOrder view Product Details FrontEnd

--- app/Http/Controllers/User/AllUserController.php
+++ app/Http/Controllers/User/AllUserController.php
@@ -18,13 +18,13 @@ use App\Mail\orderMail;
 class AllUserController extends Controller
 {
     public function MyOrders(){
-        $orders = Order::with('division','district','state','user')->where('user_id',Auth::id())->orderBy('id','DESC')->get();
+        $orders = Order::where('user_id',Auth::id())->orderBy('id','DESC')->get();
         return view('frontend.user.order.order_view',compact('orders'));
     }
 
     public function OrderDetails($order_id){
-        $order = Order::where('id',$order_id)->where('user_id',Auth::id())->first();
-        $orderItem = OrderItem::where('order_id',$order_id)->orderBy('id','DESC')->get();
+        $order = Order::with('division','district','state','user')->where('id',$order_id)->where('user_id',Auth::id())->first();
+        $orderItem = OrderItem::with('product')->where('order_id',$order_id)->orderBy('id','DESC')->get();
 
         return view('frontend.user.order.order_details',compact('order','orderItem'));
 
